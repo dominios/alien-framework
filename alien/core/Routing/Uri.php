b220afb3ee07749a7ca2cd76c2ecf080e1c5baf2
Dominik <domgersak@gmail.com>
2015-08-18 18:09:28 +0200

    added getters and setters to URI

--- alien/core/Routing/Uri.php
+++ alien/core/Routing/Uri.php
@@ -37,31 +37,256 @@ use Alien\Routing\Exception\InvalidRequest;
  */
 class Uri {
 
-    protected $protocol;
-    protected $username;
-    protected $password;
-    protected $domains;
-    protected $port;
-    protected $path;
-    protected $query;
-    protected $fragment;
+    /**
+     * @var string
+     */
+    protected $protocol = "";
+
+    /**
+     * @var string
+     */
+    protected $username = "";
+
+    /**
+     * @var string
+     */
+    protected $password = "";
+
+    /**
+     * @var string
+     */
+    protected $host = "";
+
+    /**
+     * @var string
+     */
+    protected $port = "";
+
+    /**
+     * @var string
+     */
+    protected $path = "";
+
+    /**
+     * @var string
+     */
+    protected $query = "";
+
+    /**
+     * @var string
+     */
+    protected $fragment = "";
 
     public static function createFromString($string) {
         // foo://username:password@example.com:8042/over/there/index.dtb?type=animal&name=narwhal#nose
         $parts = parse_url($string);
         if($parts === false) {
-            throw new \InvalidArgumentException("String is not valid URI.");
+            throw new \InvalidArgumentException("String is not valid URI");
         }
         $uri = new self;
-        $uri->protocol = $parts['scheme'];
-        $uri->username = $parts['user'];
-        $uri->password = $parts['pass'];
-        $uri->domains = explode('.', $parts['host']);
-        $uri->port = $parts['port'];
-        $uri->path = $parts['path'];
-        $uri->query = $parts['query'];
-        $uri->fragment = $parts['fragment'];
+        if(array_key_exists('scheme', $parts)) {
+            $uri->protocol = $parts['scheme'];
+        }
+        if(array_key_exists('user', $parts)) {
+            $uri->username = $parts['user'];
+        }
+        if(array_key_exists('pass', $parts)) {
+            $uri->password = $parts['pass'];
+        }
+        if(array_key_exists('host', $parts)) {
+            $uri->host = $parts['host'];
+        }
+        if(array_key_exists('port', $parts)) {
+            $uri->port = $parts['port'];
+        }
+        if(array_key_exists('path', $parts)) {
+            $uri->path = $parts['path'];
+        }
+        if(array_key_exists('query', $parts)) {
+            $uri->query = $parts['query'];
+        }
+        if(array_key_exists('fragment', $parts)) {
+            $uri->fragment = $parts['fragment'];
+        }
         return $uri;
+    }
+
+    /**
+     * @return string
+     */
+    public function getProtocol()
+    {
+        return $this->protocol;
+    }
+
+    /**
+     * @param string $protocol
+     * @return Uri
+     */
+    public function setProtocol($protocol)
+    {
+        $this->protocol = $protocol;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param string $username
+     * @return Uri
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $password
+     * @return Uri
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getHost()
+    {
+        return $this->host;
+    }
+
+    /**
+     * @param string $host
+     * @return Uri
+     */
+    public function setHost($host)
+    {
+        $this->host = $host;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPort()
+    {
+        return $this->port;
+    }
+
+    /**
+     * @param string $port
+     * @return Uri
+     */
+    public function setPort($port)
+    {
+        $this->port = $port;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPath()
+    {
+        return $this->path;
+    }
+
+    /**
+     * @param string $path
+     * @return Uri
+     */
+    public function setPath($path)
+    {
+        $this->path = $path;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getQuery()
+    {
+        return $this->query;
+    }
+
+    /**
+     * @param string $query
+     * @return Uri
+     */
+    public function setQuery($query)
+    {
+        $this->query = $query;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFragment()
+    {
+        return $this->fragment;
+    }
+
+    /**
+     * @param string $fragment
+     * @return Uri
+     */
+    public function setFragment($fragment)
+    {
+        $this->fragment = $fragment;
+        return $this;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getDomains()
+    {
+        return strlen($this->host) ? explode('.', $this->host) : "";
+    }
+
+    /**
+     * Returns key => value array of query (GET) parameters from URI
+     * @return array
+     */
+    public function getParams()
+    {
+        if(strlen($this->query)) {
+            if(strpos($this->query, '&') !== false) {
+                $pairs = explode('&', $this->query);
+                $ret = [];
+                foreach ($pairs as $pair) {
+                    $keyValue = explode('=', $pair);
+                    $ret[$keyValue[0]] = $keyValue[1];
+                }
+                return $ret;
+            } else {
+                $keyValue = explode('=', $this->query);
+                return [
+                    (string) $keyValue[0] => $keyValue[1]
+                ];
+            }
+        } else {
+            return [];
+        }
     }
 
 }
--- test/core/Routing/UriTest.php
+++ test/core/Routing/UriTest.php
@@ -1,13 +1,95 @@
 <?php
 
+use Alien\Routing\Uri;
+
 require_once __DIR__ . '/../../../alien/core/Routing/Uri.php';
 
 class UriTest extends PHPUnit_Framework_TestCase {
 
+    private $correctFullUrl;
+
+    public function setUp() {
+        $this->correctFullUrl = "foo://username:password@example.com:8042/over/there/index.dtb?type=animal&name=narwhal#&nose";
+    }
+
     public function testValidCreationFromString() {
-        $uri = "foo://username:password@example.com:8042/over/there/index.dtb?type=animal&name=narwhal#&nose";
-        $test = \Alien\Routing\Uri::createFromString($uri);
+        $test = Uri::createFromString($this->correctFullUrl);
         $this->assertInstanceOf('\Alien\Routing\Uri', $test);
+    }
+
+    public function testGetProtocol() {
+        $uri = Uri::createFromString($this->correctFullUrl);
+        $this->assertEquals('foo', $uri->getProtocol());
+    }
+
+    public function testGetUsername() {
+        $uri = Uri::createFromString($this->correctFullUrl);
+        $this->assertEquals('username', $uri->getUsername());
+    }
+
+    public function testGetPassword() {
+        $uri = Uri::createFromString($this->correctFullUrl);
+        $this->assertEquals('password', $uri->getPassword());
+    }
+
+    public function testGetHost() {
+        $uri = Uri::createFromString($this->correctFullUrl);
+        $this->assertEquals('example.com', $uri->getHost());
+    }
+
+    public function testGetDomains() {
+        $uri = Uri::createFromString($this->correctFullUrl);
+        $domains = ['example', 'com'];
+        $this->assertSame($domains, $uri->getDomains());
+    }
+
+    public function testGetPort() {
+        $uri = Uri::createFromString($this->correctFullUrl);
+        $this->assertEquals('8042', $uri->getPort());
+    }
+
+    public function testGetPath() {
+        $uri = Uri::createFromString($this->correctFullUrl);
+        $this->assertEquals('/over/there/index.dtb', $uri->getPath());
+    }
+
+    public function testGetQuery() {
+        $uri = Uri::createFromString($this->correctFullUrl);
+        $this->assertEquals('type=animal&name=narwhal', $uri->getQuery());
+    }
+
+    public function testGetParams() {
+        $uri = Uri::createFromString($this->correctFullUrl);
+        $params = [
+            'type' => 'animal',
+            'name' => 'narwhal'
+        ];
+        $this->assertSame($params, $uri->getParams(), 'Failed to get params when multiple params in URI');
+
+        $uri2 = Uri::createFromString('example.com?x=y', 'Failed to get params when only one pair of params in URI');
+        $this->assertSame(['x' => 'y'], $uri2->getParams());
+
+        $uri3 = Uri::createFromString('example.com', 'Failed to get params when no params in URI');
+        $this->assertSame([], $uri3->getParams());
+
+    }
+
+    public function testGetFragment() {
+        $uri = Uri::createFromString($this->correctFullUrl);
+        $this->assertEquals('&nose', $uri->getFragment());
+    }
+
+    public function testRelativeUrl() {
+        $uri = Uri::createFromString("/www/example/www");
+        $this->assertEquals('/www/example/www', $uri->getPath());
+        $this->assertEmpty($uri->getProtocol());
+        $this->assertEmpty($uri->getUsername());
+        $this->assertEmpty($uri->getPassword());
+        $this->assertEmpty($uri->getHost());
+        $this->assertEmpty($uri->getDomains());
+        $this->assertEmpty($uri->getQuery());
+        $this->assertEmpty($uri->getParams());
+        $this->assertEmpty($uri->getFragment());
     }
 
 }
