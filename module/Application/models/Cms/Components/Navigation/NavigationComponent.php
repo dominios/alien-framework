2478c48da36aa539679a33c59d4f8a5a7a3c9269
Dominik <domgersak@gmail.com>
2015-10-14 22:37:44 +0200

    basic navbar angular editing

--- module/Application/models/Cms/Components/Navigation/NavigationComponent.php
+++ module/Application/models/Cms/Components/Navigation/NavigationComponent.php
@@ -6,7 +6,58 @@ use DOMDocument;
 
 class NavigationComponent extends \Alien\Mvc\Component\NavigationComponent {
 
+    public function cmsRender()
+    {
+        ?>
+            <nav ng-controller="navbarCtrl">
+                <div>
+                    <button class="btn btn-primary" ng-show="!isEditing" ng-click="setToEditMode();">
+                        <i class="fa fa-pencil"></i> Začať úpravy
+                    </button>
+                    <button class="btn btn-success" ng-show="isEditing" ng-click="saveEditing();">
+                        <i class="fa fa-check"></i> Uložiť
+                    </button>
+                    <button class="btn btn-danger" ng-show="isEditing" ng-click="cancelEditMode();">
+                        <i class="fa fa-ban"></i> Zrušiť
+                    </button>
+                    <button class="btn btn-primary" ng-show="isEditing" ng-click="createNewLink();">
+                        <i class="fa fa-plus-circle"></i> Pridať link
+                    </button>
+                </div>
+                <ul class="nav nav-justified editable">
+                    <li ng-repeat="link in links">
+                        <a href="{{link.link}}" ng-show="!isEditing">{{link.label}}</a>
+                        <a href="#" ng-show="isEditing" ng-click="editLink(link);"><i class="fa fa-pencil"></i> {{link.label}}</a>
+                    </li>
+                </ul>
+                <div ng-show="isLinkEditing">
+                    <form>
+                        <fieldset>
+                            <label>
+                                Text: <input type="text" ng-model="editingLink.label">
+                            </label>
+                            <label>
+                                Link: <input type="text" ng-model="editingLink.link">
+                            </label>
+                            <button class="btn btn-success" ng-click="saveLinkEdit();">
+                                <i class="fa fa-check"></i> Uložiť
+                            </button>
+                            <button class="btn btn-danger" ng-click="cancelLinkEdit();">
+                                <i class="fa fa-ban"></i> Zrušiť
+                            </button>
+                        </fieldset>
+                    </form>
+                </div>
+            </nav>
+        <?
+    }
+
     public function render()
+    {
+        return $this->cmsRender();
+    }
+
+    public function baseRender()
     {
         $doc = new DOMDocument('1.0');
         $ul = $doc->createElement('ul');
