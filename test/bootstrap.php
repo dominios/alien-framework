23687558cae2d6c67d3258f678a2fa5a9d9830d9
Dominik <domgersak@gmail.com>
2016-03-26 18:05:22 +0100

    fixed composer autoloading

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once __DIR__ . "/../alien/core/Psr4Autoloader.php";
+require_once __DIR__ . "/../alien/Psr4Autoloader.php";
 
 $loader = new \Alien\Psr4Autoloader();
 $loader->register();
-$loader->addNamespace('Alien', __DIR__ . "/../alien/core/");
+$loader->addNamespace('Alien', __DIR__ . "/../alien/");
